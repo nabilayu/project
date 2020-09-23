c009b392a561daee85b556a5994cce5b998785fa
nabilayu <nabilaay7@gmail.com>
2020-09-24 03:25:12 +0700

    second

---
+++ biodata.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	
+	printf("\n");
+	printf("|================================BIODATA DIRI==============================|\n");
+	printf("|								  	   |\n");
+	printf("|1. Nama : Nabila Ayu Puspita	  					   |  \n");
+	printf("|								  	   |\n");
+	printf("|2. Tempat Tanggal Lahir : Balikpapan, 18 Maret 2001	  		   |\n");
+	printf("|									   |\n");
+	printf("|									   |\n");
+	printf("|3. Jenis Kelamin : Perempuan		  				   |\n");
+	printf("|									   |\n");
+	printf("|4. Alamat : Jalan Bukit Pelajar 1 Rt.45 No.49 Balikpapan, Kalimantan Timur|\n");
+	printf("|									   |\n");
+	printf("|5. Hobi : Mendengarkan musik		  				   |\n");
+	printf("|									   |\n");
+	printf("|6. Pendidikan : Mahasiswa Aktif UPN Veteran Jatim			   |\n");
+	printf("|									   |\n");
+	printf("|==========================================================================|");
+
+	return 0;
+}
